3472e0c939f1c8f6ebdde7066d00293f19e22678
vyacheslav <vyacheslav@gmail.com>
2020-06-23 16:57:37 +0300

    9 initial commit

--- src/main/java/lesson5/app/Invoke.java
+++ src/main/java/lesson5/app/Invoke.java
@@ -4,5 +4,13 @@ public class Invoke {
     public static void main(String[] args) {
         Server server = new Server();
         System.out.println(server.getUsers());
+
+        String actual = server.getUsers();
+        String expected = "Vova, Andrey, Alexandr----";
+        if (!actual.equals(expected)) {
+            System.out.println("Test failed");
+            throw new Error();
+        }
+        System.out.println("After error");
     }
 }
---
+++ src/test/java/lesson5/FirstTest.java
@@ -0,0 +1,12 @@
+package test.java.lesson5;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public static void successLogin() {
+        System.out.println("Hello");
+
+    }
+}
