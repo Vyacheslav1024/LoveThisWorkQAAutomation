729fe8f409ec1d987744f38fc8cb682e4a40d9bf
vyacheslav <vyacheslav@gmail.com>
2020-06-26 00:14:24 +0300

    17 initial commit

---
+++ src/test/java/Homework/WebDriver/Phone.java
@@ -0,0 +1,8 @@
+package test.java.Homework.WebDriver;
+
+public class Phone {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+    }
+}
